36129a5a69571647fcafff5081c17c5367f5cfd7
Benjamin <bemmamin@live.dk>
2020-06-24 17:20:55 +0200

    1.10.4 - Matrix Fix.

--- src/main/java/com/benzoft/gravitytubes/hooks/MatrixHook.java
+++ src/main/java/com/benzoft/gravitytubes/hooks/MatrixHook.java
@@ -1,14 +1,14 @@
 package com.benzoft.gravitytubes.hooks;
 
 import me.rerere.matrix.api.HackType;
-import me.rerere.matrix.api.MatrixAPI;
+import me.rerere.matrix.api.MatrixAPIProvider;
 import org.bukkit.entity.Player;
 
 public class MatrixHook extends AntiCheatHook {
 
     @Override
     public void setExempt(final Player player, final boolean exempt) {
-        MatrixAPI.setEnable(HackType.FLY, exempt);
+        MatrixAPIProvider.getAPI().setEnable(HackType.FLY, exempt);
         if (exempt) {
             getExemptedPlayers().add(player.getUniqueId());
         } else getExemptedPlayers().remove(player.getUniqueId());
