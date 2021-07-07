591ea92461798563f5554789bce07f81734e473e
Benjamin <bemmamin@live.dk>
2021-07-07 19:02:38 +0200

    1.11.2 - Better HEX color support

--- src/main/java/com/benzoft/gravitytubes/utils/CustomJSONParser.java
+++ src/main/java/com/benzoft/gravitytubes/utils/CustomJSONParser.java
@@ -1,16 +1,17 @@
 package com.benzoft.gravitytubes.utils;
 
-import net.md_5.bungee.api.chat.*;
+import lombok.RequiredArgsConstructor;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 
+@RequiredArgsConstructor
 final class CustomJSONParser {
 
     private final BaseComponent mainComponent = new TextComponent("");
     private final String message;
     private int stringProgress = 0;
-
-    CustomJSONParser(final String message) {
-        this.message = message;
-    }
 
     //Translate message events. ["Message","/Command"] | ["Message","Hover"] | ["Message","Command","Hover"]
     BaseComponent parseMessage() {
@@ -20,17 +21,18 @@ final class CustomJSONParser {
 
             if (eventStart == -1 || eventEnd == -1) {
                 if (!message.substring(stringProgress == 0 ? 0 : stringProgress + 2).isEmpty()) {
-                    mainComponent.addExtra(message.substring(stringProgress == 0 ? 0 : stringProgress + 2));
+                    mainComponent.addExtra(new TextComponent(TextComponent.fromLegacyText(message.substring(stringProgress == 0 ? 0 : stringProgress + 2))));
                 }
                 break;
             }
 
-            mainComponent.addExtra(message.substring(stringProgress == 0 ? 0 : stringProgress + 2, eventStart));
+            mainComponent.addExtra(new TextComponent(TextComponent.fromLegacyText(message.substring(stringProgress == 0 ? 0 : stringProgress + 2, eventStart))));
             stringProgress = eventEnd;
 
             final String[] split = message.substring(eventStart + 2, eventEnd).split("\",\"");
-            final TextComponent extra = new TextComponent();
+            String message = "";
             String command = "";
+            String hover = "";
             boolean suggest = false;
             for (int i = 0; i < split.length; i++) {
                 if (i != 0) {
@@ -38,14 +40,13 @@ final class CustomJSONParser {
                         command = split[i];
                     } else if (split[i].equalsIgnoreCase("Suggest")) {
                         suggest = true;
-                    } else extra.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(split[i]).create()));
-                } else extra.setText(split[i]);
+                    } else hover = split[i];
+                } else message = split[i];
             }
-            if (!command.isEmpty()) {
-                extra.setClickEvent(new ClickEvent(suggest ? ClickEvent.Action.SUGGEST_COMMAND : ClickEvent.Action.RUN_COMMAND, command));
-            }
-
-            mainComponent.addExtra(extra);
+            final TextComponent textComponent = new TextComponent(TextComponent.fromLegacyText(message));
+            textComponent.setClickEvent(!command.isEmpty() ? new ClickEvent(suggest ? ClickEvent.Action.SUGGEST_COMMAND : ClickEvent.Action.RUN_COMMAND, command) : null);
+            textComponent.setHoverEvent(!hover.isEmpty() ? new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText(hover)) : null);
+            mainComponent.addExtra(textComponent);
         }
 
         return mainComponent;
