48c9b85444344f336e2f4285e9649a5f562f867b
Benjamin <bemmamin@live.dk>
2021-07-20 11:05:03 +0200

    1.11.3 - Matrix Support Fix.

--- src/main/java/com/benzoft/gravitytubes/hooks/MatrixHook.java
+++ src/main/java/com/benzoft/gravitytubes/hooks/MatrixHook.java
@@ -1,25 +1,28 @@
 package com.benzoft.gravitytubes.hooks;
 
 import me.rerere.matrix.api.HackType;
-import me.rerere.matrix.api.MatrixAPIProvider;
+import me.rerere.matrix.api.events.PlayerViolationEvent;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class MatrixHook extends AntiCheatHook {
 
-    private final HackType hackType;
+    private final Set<HackType> hackTypes = new HashSet<>();
 
     public MatrixHook() {
         // Circumventing the changes to the HackType enum in V6 and forward in Matrix.
         final boolean preV6 = Bukkit.getServer().getPluginManager().getPlugin("Matrix").getDescription().getVersion().charAt(0) < '6';
-        hackType = preV6 ? HackType.FLY : HackType.valueOf("MOVE");
+        hackTypes.add(preV6 ? HackType.FLY : HackType.valueOf("MOVE"));
+        hackTypes.add(HackType.VELOCITY);
     }
 
-    @Override
-    public void setExempt(final Player player, final boolean exempt) {
-        MatrixAPIProvider.getAPI().setEnable(hackType, exempt);
-        if (exempt) {
-            getExemptedPlayers().add(player.getUniqueId());
-        } else getExemptedPlayers().remove(player.getUniqueId());
+    @EventHandler
+    public void onViolation(final PlayerViolationEvent event) {
+        if (hackTypes.contains(event.getHackType()) && getExemptedPlayers().contains(event.getPlayer().getUniqueId())) {
+            event.setCancelled(true);
+        }
     }
 }
