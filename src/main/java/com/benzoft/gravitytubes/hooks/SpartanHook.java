5b71cecfa1439643c3ac522d7afe47df8e5cee8c
Benjamin <bemmamin@live.dk>
2022-02-06 16:36:53 +0100

    1.11.4 - Spartan Support Fix.

--- src/main/java/com/benzoft/gravitytubes/hooks/SpartanHook.java
+++ src/main/java/com/benzoft/gravitytubes/hooks/SpartanHook.java
@@ -4,16 +4,20 @@ import me.vagdedes.spartan.api.API;
 import me.vagdedes.spartan.system.Enums;
 import org.bukkit.entity.Player;
 
+import java.util.stream.Stream;
+
 public class SpartanHook extends AntiCheatHook {
+
+    private static final Enums.HackType HACK_TYPE = Stream.of(Enums.HackType.values()).anyMatch(ht -> ht.toString().equals("Fly")) ? Enums.HackType.Fly : Enums.HackType.IrregularMovements;
 
     @Override
     public void setExempt(final Player player, final boolean exempt) {
         if (exempt) {
             getExemptedPlayers().add(player.getUniqueId());
-            API.stopCheck(player, Enums.HackType.Fly);
+            API.stopCheck(player, HACK_TYPE);
         } else {
             getExemptedPlayers().remove(player.getUniqueId());
-            API.startCheck(player, Enums.HackType.Fly);
+            API.startCheck(player, HACK_TYPE);
         }
     }
 }
